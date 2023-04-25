62b6005b766bf7159ef2a708a79b9afecd4d9d69
Maksim Kondrashov <konor.max@mail.ru>
2023-04-25 16:58:27 +0300

    added BunTest and BurgerTest

---
+++ src/test/java/praktikumTest/BunTest.java
@@ -0,0 +1,15 @@
+package praktikumTest;
+
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+    @Test
+    public void bunConstructorTest() {
+        Bun bun = new Bun("TEST", 103.0f);
+        assertEquals("TEST", bun.getName());
+        assertEquals(103.0f, bun.getPrice(), 0.0f);
+    }
+}
